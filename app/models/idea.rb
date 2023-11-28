5b1dc17df688d5a4c88ebaea12dc6408e345d09f
elisheva <elisheva.benisaac@gmail.com>
2023-11-28 20:59:59 +0000

    Add picture uploads

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,2 +1,5 @@
 class Idea < ApplicationRecord
+  #This file is used to store your ideas in the database and fetch the ideas to show them. 
+  #We’ll change it to tell Rails which field is a file upload
+  mount_uploader :picture, PictureUploader
 end
